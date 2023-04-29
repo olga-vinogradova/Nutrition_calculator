f49316cd50a2ea5d8953886e156d5531c05f7729
Lina Striogaite <lina.striogaite@gmail.com>
2023-04-29 23:30:35 +0300

    cleanup

--- src/NutritionCalculator.java
+++ src/NutritionCalculator.java
@@ -1,7 +1,3 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
